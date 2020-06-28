eaa6f1fb7f0c73a6e2e834a6a61bc80f2add25c6
Angela Stephens <angelastephens317@gmail.com>
2020-06-28 23:13:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative '../lib/concerns/memorable'
 class Song
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
+  # include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
