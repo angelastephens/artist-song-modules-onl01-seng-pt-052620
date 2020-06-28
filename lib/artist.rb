9403e6aed039d2671ed82a99569fffcef09acfb1
Angela Stephens <angelastephens317@gmail.com>
2020-06-28 23:55:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,8 +3,9 @@ require 'pry'
 class Artist
   
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
-  extend 
+  
   include Paramable
   
   attr_accessor :name
